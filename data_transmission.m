d8652f65d84a9787ea374defe265eda964aae903
Karol Gil <Karol Gil>
2016-01-13 19:00:40 +0100

    Dane dla Witolda

--- data_transmission.m
+++ data_transmission.m
@@ -8,21 +8,26 @@ RXFILT = comm.RaisedCosineReceiveFilter( ...
 DELAY = dsp.VariableFractionalDelay;
 SYMSYNC = comm.SymbolSynchronizer('SamplesPerSymbol',2, ....
                                   'TimingErrorDetector', 'Mueller-Muller (decision-directed)');
+                              % funkcja synchronizacji 'Mueller-Muller (decision-directed)' - parametr
 
 data = randi([0 3],1000,1);
 modSig = step(MOD, data);
 txSig = step(TXFILT,modSig);
-delaySig = step(DELAY,txSig,1/5);
+delaySig = step(DELAY,txSig,1/5); % opoznienie 1/5 - parametr
 rxSig = awgn(delaySig,30,'measured');
 rxSample = step(RXFILT,rxSig);
 rxSync = step(SYMSYNC,rxSample);
 recv = step(DEMOD, rxSync);
+
+% do koñca brzydkie hacki
+% jesli probek wyjsciowych jest wiecej niz wejscia
 if length(recv) < length(data)
     recv = [0; recv];
 end
 if length(recv) > length(data)
     recv = recv(2:end);
 end
+% przesuwamy wejscie o 11 lub 10, bo tak
 error_rate = comm.ErrorRate('ReceiveDelay',11);
 errors = step(error_rate,data,recv);
 if errors(1) > 0.5
